ee00828c53e3f4c4d83889a19f8e25e3ab428c2b
Pallav27 <pallavshrm27@gmail.com>
2024-07-02 23:28:23 +0530

    Finished Portfolio Website

---
+++ src/sections/Hero/Hero.jsx
@@ -0,0 +1,68 @@
+import styles from './HeroStyles.module.css';
+import heroImg from '../../assets/croppedme.png';
+import sun from '../../assets/sun.svg';
+import moon from '../../assets/moon.svg';
+import twitterLight from '../../assets/twitter-light.svg';
+import twitterDark from '../../assets/twitter-dark.svg';
+import githubLight from '../../assets/github-light.svg';
+import githubDark from '../../assets/github-dark.svg';
+import linkedinLight from '../../assets/linkedin-light.svg';
+import linkedinDark from '../../assets/linkedin-dark.svg';
+import CV from '../../assets/PALcv.pdf';
+import { useTheme } from '../../common/ThemeContext';
+
+function Hero() {
+  const { theme, toggleTheme } = useTheme();
+
+  const themeIcon = theme === 'light' ? sun : moon;
+  const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
+  const githubIcon = theme === 'light' ? githubLight : githubDark;
+  const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
+
+  return (
+    <section id="hero" className={styles.container}>
+      <div className={styles.colorModeContainer}>
+        <img
+          src={heroImg}
+          className={styles.hero}
+          alt="Profile picture of Harris Johnsen"
+        />
+        <img
+          className={styles.colorMode}
+          src={themeIcon}
+          alt="Color mode icon"
+          onClick={toggleTheme}
+        />
+      </div>
+      <div className={styles.info}>
+        <h1>
+          Pallav
+          <br />
+          Sharma
+        </h1>
+        <h2>Competitive Programmer</h2>
+        <h2>Web Developer</h2>
+        <span>
+          <a href="https://www.instagram.com/pallav_sharma27/" target="_blank">
+            <img src={twitterIcon} alt="Twitter icon" />
+          </a>
+          <a href="https://github.com/Pallav27" target="_blank">
+            <img src={githubIcon} alt="Github icon" />
+          </a>
+          <a href="https://www.linkedin.com/in/pallav-sharma-68174424b/" target="_blank">
+            <img src={linkedinIcon} alt="Linkedin icon" />
+          </a>
+        </span>
+        <p className={styles.description}>
+          With a passion for developing modern React web apps for commercial
+          businesses.
+        </p>
+        <a href={CV} download>
+          <button className="hover">Resume</button>
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,36 @@
+import styles from './ProjectsStyles.module.css';
+import viberr from '../../assets/newspaper.png';
+import freshBurger from '../../assets/face.png';
+import hipsster from '../../assets/sign.png';
+import ProjectCard from '../../common/ProjectCard';
+
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
+        <ProjectCard
+          src={viberr}
+          link="https://github.com/Pallav27/Retro-Newspaper"
+          h3="Retro Newspaper"
+          p="Landing Page"
+        />
+        <ProjectCard
+          src={freshBurger}
+          link="https://github.com/Pallav27/Text-To-Emoji/tree/main/TextToEmojiEncDec"
+          h3="Texmoji"
+          p="Text-to-Emoji Converter"
+        />
+        <ProjectCard
+          src={hipsster}
+          link="https://github.com/Pallav27/SignSave"
+          h3="Sign&Save"
+          p="Digital Signature Website"
+        />
+        
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
